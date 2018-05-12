a3f2d128efd3f4e2c9cc5a9ce051c10afcc8ea9d
Anuja Saraf <anujasaraf@Anujas-MacBook-Air.local>
2018-05-12 18:24:24 -0400

    Story 3 – Show an error if the body is invalid

--- src/main/java/net/messaging/Main.java
+++ src/main/java/net/messaging/Main.java
@@ -18,6 +18,9 @@ public class Main {
 
     public static void main(String... args) {
         try {
+            if (args.length < 2 || args[1].isEmpty())
+                throw new RuntimeException("Cannot send an email with no body.");
+
             writeToNetwork(args[0], args[1]);
         } catch (Exception e) {
             writeToConsole(e.getMessage());
