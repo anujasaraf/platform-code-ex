cf291cae351220ff2c365aac1386e99eccfe671b
Anuja Saraf <anujasaraf@Anujas-MacBook-Air.local>
2018-05-17 23:35:45 -0500

    fixing bugs and code improvements

--- src/main/java/net/messaging/Main.java
+++ src/main/java/net/messaging/Main.java
@@ -1,10 +1,18 @@
 package net.messaging;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class Main {
+    private static class CodeExException extends Exception {
+        public CodeExException(String message) {
+            super(message);
+        }
+    }
+
     private final static Logger log = Logger.getLogger(Main.class.getName());
     private static Writer network;
     private static Writer console;
@@ -20,19 +28,19 @@ public class Main {
     public static void main(String... args) {
         try {
             if (args.length < 2 || args[1].indexOf('-') == 0 && args.length < 3 || args[args.length - 1].isEmpty())
-                throw new RuntimeException("Cannot send an email with no body.");
+                throw new CodeExException("Cannot send an email with no body.");
 
             boolean isIm = args[0].contains("-im");
             String addresses[];
             if (!isIm) {
                 addresses = Arrays.copyOf(args, args.length - 1);
             } else {
-                addresses = Arrays.copyOfRange(args, 1,args.length - 1);
+                addresses = Arrays.copyOfRange(args, 1 ,args.length - 1);
             }
 
             String message = args[args.length - 1];
             writeToNetwork(addresses, message, isIm);
-        } catch (Exception e) {
+        } catch (CodeExException e) {
             writeToConsole(e.getMessage());
         }
     }
@@ -46,7 +54,8 @@ public class Main {
         }
     }
 
-    private static void validate(String[] addresses) {
+    private static List<String> validate(String[] addresses) throws CodeExException {
+        List<String> validAddresses = new ArrayList<>();
         StringBuilder invalidAddress = new StringBuilder();
         int count = 0;
         for (String address : addresses) {
@@ -55,30 +64,29 @@ public class Main {
                 if (email.indexOf('@') <= 0) {
                     invalidAddress.append(" ").append(email);
                     count++;
+                    continue;
                 }
+
+                validAddresses.add(email);
             }
         }
 
-        if (count == 1) {
-            throw new RuntimeException("Invalid email address:" + invalidAddress);
-        } else if (count > 1) {
-            throw new RuntimeException("Invalid email addresses:" + invalidAddress);
+        if (count != 0) {
+            throw new CodeExException("Invalid email address" + (count == 1 ? ":" : "es:") + invalidAddress);
         }
+
+        return validAddresses;
     }
 
-    private static void writeToNetwork(String[] addresses, String message, boolean isIm) {
-        validate(addresses);
+    private static void writeToNetwork(String[] addresses, String message, boolean isIm) throws CodeExException {
+        List<String> emailIds = validate(addresses);
         StringBuilder email = new StringBuilder();
         email.append("connect ").append(isIm ? "chat" : "smtp");
-
-        for (String address : addresses) {
-            String[] emails = address.split(",");
-            for (String emid : emails) {
-                if (!isIm)
-                    email.append("\nTo: ").append(emid);
-                else
-                    email.append("\n<" + emid+ ">").append("(" + message + ")");
-            }
+        for (String emid : emailIds) {
+            if (!isIm)
+                email.append("\nTo: ").append(emid);
+            else
+                email.append("\n<" + emid+ ">").append("(" + message + ")");
         }
 
         if (!isIm) {
